cb91650b3512dd0941f91ba000548f09f31bfba7
satya-patel <satyaprakashpatel1998@gmail.com>
2021-05-25 13:24:23 +0530

    if and else2 added

---
+++ ifandelse2.js
@@ -0,0 +1,12 @@
+{
+    let month = 6
+    let day = 25
+    if(day<=31&&month<=12){
+        if(day>20&&month>=3&&month<=6)
+            console.log("True");
+        else 
+            console.log("False");
+    }
+    else 
+        console.log("Invalid Date")
+} 
